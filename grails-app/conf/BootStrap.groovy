c65f9e0a8ec0284bcbd2cd52606e3092c7567832
dave <dave@bould.net>
2017-10-21 12:35:31 +0100

    Added initialisation data

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,11 @@
+import secodingchallenge.Account
+
 class BootStrap {
 
     def init = { servletContext ->
+        new Account(name: "Sam", email: "dbould@gmail.com", balance: 200).save()
+        new Account(name: "Alex", email: "dbould@gmail.com", balance: 200).save()
+        new Account(name: "Frankie", email: "dbould@gmail.com", balance: 200).save()
     }
     def destroy = {
     }
