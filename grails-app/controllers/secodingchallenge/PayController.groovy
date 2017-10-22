10dcc9d1f59e8e1aad8ea1a179e344555ebe0af3
dave <dave@bould.net>
2017-10-22 22:04:23 +0100

    Added code for email sending Commented out to keep front end working

--- grails-app/controllers/secodingchallenge/PayController.groovy
+++ grails-app/controllers/secodingchallenge/PayController.groovy
@@ -1,8 +1,11 @@
 package secodingchallenge
 
+//import grails.plugin.mail.GrailsMailException
+
 class PayController {
     def accountService
     def payService
+    //def mailService
 
     def index() {
         def accounts = Account.list()
@@ -20,14 +23,29 @@ class PayController {
         ).save()
 
         def fromAccount = Account.get(params.fromAccount)
+
         Double amount = params.double('amount')
 
         def approveTransaction = payService.approveTransaction(fromAccount.balance, amount)
 
         if (approveTransaction == true) {
+
             accountService.updateBalances(params.int('fromAccount'),
                                                  params.int('toAccount'),
                                                  amount)
+
+//            def toAccount = Account.get(params.toAccount)
+//            try {
+//                mailService.sendMail {
+//                    to fromAccount.email, toAccount.email
+//                    from "my@bank.com"
+//                    subject "Account Transfer Complete"
+//                    text "£" + amount + " has been transferred from " + fromAccount.name + " to " + toAccount.name
+//                }
+//            } catch (GrailsMailException e) {
+//
+//            }
+
         }
 
         render view: "pay", model: [accounts: accounts,
